07225c3de764980a9f6ec62f68d2e9bb1e540d2e
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-06-02 02:33:52 +0000

    done

--- twake/frontend/src/index.tsx
+++ twake/frontend/src/index.tsx
@@ -10,3 +10,15 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+// After the App component is rendered
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
